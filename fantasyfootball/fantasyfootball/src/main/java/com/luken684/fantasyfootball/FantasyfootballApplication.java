26fdea6ca78eaaa24970558f2f0fc6073151713e
luken684 <61052690+luken684@users.noreply.github.com>
2020-02-14 20:45:33 +0000

    Setup commit
    all things setup except h2 database

---
+++ fantasyfootball/fantasyfootball/src/main/java/com/luken684/fantasyfootball/FantasyfootballApplication.java
@@ -0,0 +1,13 @@
+package com.luken684.fantasyfootball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyfootballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyfootballApplication.class, args);
+	}
+
+}
---
+++ fantasyfootball/fantasyfootball/src/test/java/com/luken684/fantasyfootball/FantasyfootballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luken684.fantasyfootball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyfootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
